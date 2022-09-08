348dd4b3f4ead94349ab1f1b85f71dd2012d16a4
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-09-08 19:15:34 +0530

    The lonely mode swtich and computer move maker is added

--- script.js
+++ script.js
@@ -13,20 +13,37 @@ const displayControl = (function(document) {
     function markActiveSymbol() {
         if(this.textContent == "" && gameControl.getHumanPlayerSymbol() != "") {
             console.log(this.getAttribute('data-id'));
-            let currentPlayer = mainGameContorl.getActivePlayerSymbol();
-            this.textContent = currentPlayer.symbol;
+            let currentPlayerSymbol = mainGameContorl.getActivePlayerSymbol();
+            this.textContent = currentPlayerSymbol;
             // call the other fucntion which in turn call the reset function which are required on filling of a position in board.
-            eventFireAtBoradBoxClick(currentPlayer, this.getAttribute('data-id'));
+            eventFireAtBoradBoxClick(currentPlayerSymbol, this.getAttribute('data-id'));
+            makeComputerPlayerMark();
+            // gameBoardLogic.getEmptyDivId();
         } 
     }
 
+    function makeComputerPlayerMark() {
+        if(gameControl.getLonelyMode() == false) {
+            return;
+        }
+        let divId = gameBoardLogic.getEmptyDivId(); 
+        let div = document.querySelector(`div[data-id="${divId}"]`);
+        if(div != null && div.textContent == "")  {
+            console.log(div.textContent);
+            let currentPlayerSymbol = mainGameContorl.getActivePlayerSymbol();
+            div.textContent = currentPlayerSymbol;
+            eventFireAtBoradBoxClick(currentPlayerSymbol, divId);
+        }
+    }
+
     // calls the necessary fucntions when a position in the box is marked
-    function eventFireAtBoradBoxClick(player, divId) {
+    function eventFireAtBoradBoxClick(playerSymbol, divId) {
         console.log('necessary events fired');
         setLastDivId(divId);
-        gameBoardLogic.populateMainBoard(player);
-        if(gameBoardLogic.checkBoardFilled() == true) {
-            gameControl.reset();
+        gameBoardLogic.populateMainBoard(playerSymbol);
+        let winCheck = gameBoardLogic.checkWinner();
+        if(gameBoardLogic.checkBoardFilled() == true && winCheck == false) {
+            gameControl.setGameOverMsg("Game is tied");
         }
     }
 
@@ -55,6 +72,10 @@ const displayControl = (function(document) {
 })(document);
 
 const gameControl = (function(document) {
+
+    // store the choice of the human player symbol
+    let humanPlayerSymbol = "";
+    let lonelyMode = false;
     
     // get the buttons used in the game to choose the symbol
     const buttons = Array.from(document.querySelectorAll('#symbol-choice > button'));
@@ -62,13 +83,11 @@ const gameControl = (function(document) {
     // selecting and adding a event listnere to the restart button 
     document.getElementById("restart").addEventListener("click", reset);
 
+    document.querySelector("input").addEventListener("change", setLonelyMode);
 
     // adding eventlistener to the control buttons
     buttons.forEach((button) => button.addEventListener('click', setHumanPlayerSymbol));
 
-    // store the choice of the human player symbol
-    let humanPlayerSymbol = "";
-    
     // setting the human player symbol according to the textcontent of the button if only the human player symbol is empty
     function setHumanPlayerSymbol() {
         if(displayControl.getLastDivId() == "") {
@@ -81,12 +100,56 @@ const gameControl = (function(document) {
         }
     }
 
+    function setLonelyMode() {
+        if(displayControl.getLastDivId() == "") {
+            if(lonelyMode) {
+                lonelyMode = false;
+            } else {
+                lonelyMode = true;
+            }
+        } else {
+            console.log("Lonely mode can only be change if no move have been made");
+            this.checked = lonelyMode;
+        }
+    }
+
+    const getLonelyMode = function() {
+        return lonelyMode;
+    }
+
+    const setGameOverMsg = function(message) {
+        let div = document.getElementById('game-over');
+        if(div.hasChildNodes()) {
+            return;
+        }
+        div.addEventListener("click", reset);
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        p1.textContent = "GAME OVER";
+        p2.textContent = message;
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.style.transform = 'scale(1)';
+        div.style.transform = 'translate(-50%, -50%)';
+    }
+
+    function resetGameOverMsg() {
+        let div = document.getElementById('game-over');
+        if(div.hasChildNodes()) {
+            div.removeChild(div.firstChild);
+            div.removeChild(div.firstChild);
+            div.removeEventListener('click', reset);
+            div.style.transform = 'scale(0)';
+        }
+    }
+
     function reset() {
         console.log('restart button was pressed');
         humanPlayerSymbol = "";
+        resetGameOverMsg();
         displayControl.resetDisplay();
         mainGameContorl.resetMainGameControl();
-        gameBoardLogic.resetMainGameBoardLogic();
+        gameBoardLogic.initGameBoardLogic();
     }
 
     // return the human player sysmbol
@@ -96,14 +159,20 @@ const gameControl = (function(document) {
 
     return {
         getHumanPlayerSymbol,
-        reset
+        getLonelyMode,
+        setGameOverMsg
     };
 
 })(document);
 
 
 const Player = function(name, symbol, playerNumber) {
-    return {name, symbol, playerNumber};
+    const equals = function(player) {
+        if(this.name == player.name && this.symbol == player.symbol) {
+            return true;
+        }
+    };
+    return {name, symbol, playerNumber, equals};
 };
 
 const mainGameContorl = (function() {
@@ -134,7 +203,7 @@ const mainGameContorl = (function() {
     // returns the player whose chance is next to make a move on the board
     const getActivePlayerSymbol = function() {
         console.log('get active work player works');
-        return(activePlayerSeries.pop());
+        return(activePlayerSeries.pop().symbol);
     };
 
     const initMainGameControl = function() {
@@ -142,6 +211,17 @@ const mainGameContorl = (function() {
         setGamePlayers();
         setActivePlayerSeries();
     };
+
+    let getWinnerPlayer = function(col, row, diag) {
+        let symbol = col != 1 ? col : row != 1 ? row : diag != 1 ? diag : false;
+
+        console.log(symbol);
+        if(symbol == player1.symbol) {
+            return player1;
+        } else {
+            return player2;
+        }
+    }; 
 
     const resetMainGameControl = function() {
         player1 = undefined;
@@ -163,6 +243,7 @@ const mainGameContorl = (function() {
         getActivePlayerSymbol,
         initMainGameControl,
         getActivePlayerSymbol,
+        getWinnerPlayer,
         resetMainGameControl,
         printPlayer,
         printActivePlayerSeries,
@@ -173,32 +254,67 @@ const mainGameContorl = (function() {
 const gameBoardLogic = (function() {
     let mainBoard;
 
-    const populateMainBoard = function(player) {
+    const populateMainBoard = function(playerSymbol) {
         let divId = displayControl.getLastDivId();
         divId = divId.split('');
         let [row, col] = divId; 
-        mainBoard[row][col] = player;
+        mainBoard[row][col] = playerSymbol;
     };
 
     const initGameBoardLogic = function() {
         console.log("the game board is initialized");
-        mainBoard = [[],[],[]];
+        mainBoard = [[1,1,1],[1,1,1],[1,1,1]];
     };
 
-    const checkWhichPlayerWin = function() {
+    const checkWinner = function() {
+        let col = checkCol();
+        let row = checkRow();
+        let diag = checkDiag();
+        console.log(col + " " + row + " " + diag);
 
-    };
+        if(col != '1' || row != '1' || diag != '1') {
+            let winPlayer = mainGameContorl.getWinnerPlayer(col, row, diag);
+            console.log(winPlayer);
+            gameControl.setGameOverMsg(`${winPlayer.name} wins`);
+            return true;
+        }
+        return false;
+    }
+
+    function checkCol() {
+        for(let i=0; i<mainBoard.length; i++) {
+            if(mainBoard[0][i] == mainBoard[1][i] && mainBoard[1][i] == mainBoard[2][i] && mainBoard[0][i] != 1) {
+                return mainBoard[0][i];
+            }
+        }
+        return 1;
+    }
+
+    function checkRow() {
+        for(let i=0; i<mainBoard.length; i++) {
+            if(mainBoard[i][0] == mainBoard[i][1] && mainBoard[i][1] == mainBoard[i][2] && mainBoard[i][0] != 1) {
+                return mainBoard[i][0];
+            }
+        }
+        return 1;
+    }
+
+    function checkDiag() {
+        if(
+            mainBoard[0][0] == mainBoard[1][1] && mainBoard[1][1] == mainBoard[2][2] && mainBoard[0][0] != 1 ||
+            mainBoard[0][2] == mainBoard[1][1] && mainBoard[1][1] == mainBoard[2][0] && mainBoard[0][2] != 1
+        ) {
+            return mainBoard[1][1];
+        }
+        return 1;
+    }
 
     const checkBoardFilled = function() {
-        if(!(mainBoard instanceof Array)) {
-            return false;
-        }
-
         let count = 0;
 
         for(let i=0; i<mainBoard.length; i++) {
             for(let j=0; j<mainBoard[i].length; j++) {
-                if(mainBoard[i][j]) {
+                if(mainBoard[i][j] != 1) {
                     count = count + 1;
                 }
             }
@@ -212,10 +328,16 @@ const gameBoardLogic = (function() {
         
     };
 
-    const resetMainGameBoardLogic = function() {
-        mainBoard = undefined;
-        console.log("the main board is reset");
-    };
+    const getEmptyDivId = function() {
+        for(let i=0; i<mainBoard.length; i++) {
+            for(let j=0; j<mainBoard[i].length; j++) {
+                if(mainBoard[i][j] == "1") {
+                    console.log(i+""+j);
+                    return i+""+j;
+                }
+            }
+        }
+    }
 
     const printMainBoard = function() {
         mainBoard.forEach((row) => console.log(row));
@@ -224,8 +346,13 @@ const gameBoardLogic = (function() {
     return {
         initGameBoardLogic,
         populateMainBoard,
+        checkWinner,
         checkBoardFilled,
-        resetMainGameBoardLogic,
+        getEmptyDivId,
         printMainBoard
     }
+})();
+
+const computerPlayer = (function() {
+
 })();
