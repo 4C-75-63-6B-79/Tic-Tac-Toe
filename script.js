73487ad88d81c6e6ec1fe61634ab334be6569a76
4C-75-63-6B-79 <luckyhairybeast987654@gmail.com>
2022-09-10 20:52:50 +0530

    Added the checkwinner and the display game over message

--- script.js
+++ script.js
@@ -6,16 +6,53 @@ const displayControl = (function(document){
     function boxClicked() {
         console.log("------------------------");
         console.log(this.getAttribute('data-id') + ' is clicked.');
+        gameControl.humanMakeMove(this.getAttribute('data-id'));
+    }
+
+    function markBox(dataId) {
+        let box = document.querySelector(`div[data-id="${dataId}"]`);
+        if(box != null && box.textContent == "") {
+            box.textContent = gameControl.getActivePlayer();
+        }
     }
 
     let displayReset = function() {
         console.log('display is reset.')
         boxes.forEach((box) => box.textContent = "");
-
     };
 
+    let displayGameOverMessage = function(symbol) {
+        let div = document.getElementById('game-over');
+        if(div.hasChildNodes()) {
+            return;
+        }
+        div.addEventListener("click", resetGameOverMessage);
+        let p1 = document.createElement("p");
+        let p2 = document.createElement("p");
+        p1.textContent = "GAME OVER";
+        p2.textContent = symbol + " wins the game";
+        div.appendChild(p1);
+        div.appendChild(p2);
+        div.style.transform = 'scale(1)';
+        div.style.transform = 'translate(-50%, -50%)';
+    }
+
+    function resetGameOverMessage() {
+        let div = document.getElementById('game-over');
+        if(div.hasChildNodes()) {
+            div.removeChild(div.firstChild);
+            div.removeChild(div.firstChild);
+            div.removeEventListener('click', resetGameOverMessage);
+            div.style.transform = 'scale(0)';
+        }
+        gameControl.gameControlReset();
+        displayControl.displayReset();
+    }
+
     return {
-        displayReset
+        displayReset,
+        markBox,
+        displayGameOverMessage
     };
 
 })(document);
@@ -31,6 +68,7 @@ const buttonControl = (function(document) {
         gameControl.gameControlReset();
         displayControl.displayReset();
         gameControl.setPlayers(this.textContent);
+        gameControl.makeFirstMove();
     }
 
 
@@ -38,6 +76,8 @@ const buttonControl = (function(document) {
 
 const gameControl = (function(){
     let gameBoard;
+    let humanPlayer, computerPlayer;
+    let playersMoveSeries;
 
     function initGameBoard()  {
         gameBoard = [
@@ -45,11 +85,117 @@ const gameControl = (function(){
             ['', '', ''],
             ['', '', '']
         ];
+        playersMoveSeries = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'].reverse();
+        humanPlayer = "";
+        computerPlayer = "";
         console.log("Game Board is initialized")
     };
 
     let setPlayers = function(hum_symbol) {
         console.log('players are set.');
+        humanPlayer = hum_symbol;
+        computerPlayer = hum_symbol == "X" ? 'O' : 'X';
+    };
+
+    function gameBoardPopulate(symbol, dataId) {
+        let x = dataId[0];
+        let y = dataId[1];
+        gameBoard[x][y] = symbol;
+    }
+
+    let getActivePlayer = function() {
+        // if(playersMoveSeries.length != 0) {
+            return playersMoveSeries.pop();
+        // }
+    };
+
+    function getEmpytDiv() {
+        for(let i=0; i<gameBoard.length; i++) {
+            for(let j=0; j<gameBoard[i].length; j++) {
+                if(gameBoard[i][j] == "") {
+                    return i+""+j;
+                }
+            }
+        }
+    }
+
+    let makeFirstMove = function() {
+        if(computerPlayer == "X") {
+            let emptyDiv = getEmpytDiv();
+            displayControl.markBox(emptyDiv);
+            gameBoardPopulate("X", emptyDiv);
+        }
+    };
+
+    let humanMakeMove = function(dataId) {
+        if(humanPlayer == null) {
+            return;
+        }
+        displayControl.markBox(dataId);
+        gameBoardPopulate(humanPlayer, dataId);
+        if(checkWinner()) {
+            displayControl.displayGameOverMessage(checkWinner());
+        }
+        computerMakeMove();
+    };
+
+    function computerMakeMove() {
+        if(computerPlayer == null) {
+            return;
+        }
+        let emptyDiv = getEmpytDiv();
+        displayControl.markBox(emptyDiv);
+        gameBoardPopulate(computerPlayer, emptyDiv);
+        if(checkWinner()) {
+            displayControl.displayGameOverMessage(checkWinner());
+        }
+    }
+
+    let checkWinner = function() {
+        function checkRow(symbol) {
+            for(let i=0; i<gameBoard.length; i++) {
+                if(gameBoard[i][0] == symbol && gameBoard[i][1] == symbol && gameBoard[i][2] == symbol) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    
+        function checkCol(symbol) {
+            for(let i=0; i<gameBoard.length; i++) {
+                if(gameBoard[0][i] == symbol && gameBoard[1][i] == symbol && gameBoard[2][i] == symbol) {
+                    return true;
+                }
+            }
+            return false;
+        }
+    
+        function checkDiag(symbol) {
+            if((gameBoard[0][0] == symbol && gameBoard[1][1] == symbol && gameBoard[2][2] == symbol) ||
+               (gameBoard[0][2] == symbol && gameBoard[1][1] == symbol && gameBoard[2][0] == symbol)) {
+                return true;
+            }
+            return false;
+        }
+
+        function checkTie() {
+            for(let i=0; i<gameBoard.length; i++) {
+                for(let j=0; j<gameBoard[i].length; j++) {
+                    if(gameBoard[i][j] == "") {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+
+        if(checkRow(computerPlayer) || checkCol(computerPlayer) || checkDiag(computerPlayer)) {
+            return computerPlayer;
+        } else if(checkRow(humanPlayer) || checkCol(humanPlayer) || checkDiag(humanPlayer)) {
+            return humanPlayer;
+        } else if(checkTie()) {
+            return "tie";
+        }
     }
 
     let gameControlReset = function() {
@@ -59,6 +205,11 @@ const gameControl = (function(){
 
     return {
         gameControlReset,
-        setPlayers
+        setPlayers,
+        getActivePlayer,
+        makeFirstMove,
+        humanMakeMove,
+        computerMakeMove,
+        checkWinner
     };
 })();
